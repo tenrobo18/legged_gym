3c257d8e8cd1f2a04f24ba6248b1d06d9c7ef19a
t-suzuki <t-suzuki@jsk.imi.i.u-tokyo.ac.jp>
2025-01-23 17:59:36 +0900

    armatureと直動関節のlimitを正しくした

--- legged_gym/envs/base/monolegged_robot.py
+++ legged_gym/envs/base/monolegged_robot.py
@@ -392,7 +392,7 @@ class MonoLeggedRobot(BaseTask):
                 r = self.dof_pos_limits[i, 1] - self.dof_pos_limits[i, 0]
                 self.dof_pos_limits[i, 0] = m - 0.5 * r * self.cfg.rewards.soft_dof_pos_limit
                 self.dof_pos_limits[i, 1] = m + 0.5 * r * self.cfg.rewards.soft_dof_pos_limit
-        armature = torch.tensor([4.14e-3]*3, dtype=torch.float32) #[todo] ちゃんと設定する
+        armature = torch.tensor([0.54811795 0.54811064 9.51120847], dtype=torch.float32) #[todo] ちゃんと設定する
         for i in range(props.size):
             props[i][9] = armature[i]
         return props
